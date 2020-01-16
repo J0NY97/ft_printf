aafe185df08bcb995dd21025d78adacaade11e19
J0NY97 <jony.salmi97@gmail.com>
2020-01-16 13:22:07 +0200

    addded longlong min

--- ft_llnbrlen.c
+++ ft_llnbrlen.c
@@ -19,6 +19,9 @@ int	ft_llnbrlen(long long int nbr)
 	len = 0;
 	if (nbr == 9223372036854775807)
 		return (19);
+	// could be 19 not sure how ive done it, logically 20 with -
+	if (nbr == -9223372036854775808)
+		return (20);
 	if (nbr == 0)
 		return (1);
 	if (nbr < 0)
